27419c81aced933952983f1d3a7c1a41959473b5
hromau <by.skleroz@ya.ru>
2017-11-22 12:29:46 +0300

    generic db

---
+++ DbForWS/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DbForWS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.редактированиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.редактированиеToolStripMenuItem,
+            this.toolStripComboBox1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1012, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // редактированиеToolStripMenuItem
+            // 
+            this.редактированиеToolStripMenuItem.Name = "редактированиеToolStripMenuItem";
+            this.редактированиеToolStripMenuItem.Size = new System.Drawing.Size(108, 23);
+            this.редактированиеToolStripMenuItem.Text = "Редактирование";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 322);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem редактированиеToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+    }
+}
+
---
+++ DbForWS/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DbForWS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\WebServices.mdf;Integrated Security=True;Connect Timeout=30");
+        //Thread AppExit = new Thread(CloseAndExit);
+
+        static bool CreateDB()
+        {
+            //true - enabled
+            bool c = false;
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"server=(localdb)\v11.0");
+                using (connection)
+                {
+                    connection.Open();
+                    string sql = string.Format(@"CREATE DATABASE [WebServices] ON PRIMARY (NAME=WebServices,FILENAME = '{0}\WebServices.mdf')LOG ON (NAME=WebServices_log,FILENAME = '{0}\WebServices_log.ldf')", @"|DataDirectory|\WebServices.mdf");
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                c = true;
+            }
+
+            return c;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Width = this.Width;
+            dataGridView1.Height = this.Height - 24;
+            this.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+            if (CreateDB())
+                using (SqlCommand comm = new SqlCommand("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE LIKE '%TABLE%'", connection))
+                {
+                    //try
+                    //{
+                    connection.Open();
+                    var reader = comm.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        toolStripComboBox1.Items.Add(reader.GetString(0));
+                    }
+                    //}
+                    //catch (Exception ex)
+                    //{
+                    //    MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK);
+                    //    this.Enabled = true;
+                    //    this.Cursor = Cursors.Default;
+                    //    return;
+                    //}
+
+
+                }
+            this.Enabled = true;
+            this.Cursor = Cursors.Default;
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+
+
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from " + toolStripComboBox1.SelectedItem.ToString(), connection);
+            //try
+            //{
+            dataAdapter.Fill(ds, toolStripComboBox1.SelectedItem.ToString());
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    this.Enabled = true;
+            //    this.Cursor = Cursors.Default;
+            //    connection.Close();
+            //    return;
+            //}
+
+            //try
+            //{
+            dataGridView1.DataSource = ds.Tables[toolStripComboBox1.SelectedItem.ToString()].DefaultView;
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    this.Enabled = true;
+            //    this.Cursor = Cursors.Default;
+            //    connection.Close();
+            //    return;
+            //}
+
+            this.Enabled = true;
+            this.Cursor = Cursors.Default;
+        }
+    }
+}
